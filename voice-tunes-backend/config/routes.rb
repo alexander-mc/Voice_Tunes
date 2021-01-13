90d4b58f9bcb3b3ae3366d6299919423aa3b53f4
Alexander M. Clamor <alexander.clamor@gmail.com>
2021-01-12 22:37:19 -0800

    nest routes

--- voice-tunes-backend/config/routes.rb
+++ voice-tunes-backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :recordings
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :users, only: [:index, :create, :destroy]  do
+    resources :recordings
+  end
+
 end
