17d8d4a45ee96598ac62e4fb6378ec6f6a0eb981
Alexander M. Clamor <alexander.clamor@gmail.com>
2021-01-12 22:37:39 -0800

    outline recordings controller

--- voice-tunes-backend/app/controllers/recordings_controller.rb
+++ voice-tunes-backend/app/controllers/recordings_controller.rb
@@ -1,2 +1,25 @@
 class RecordingsController < ApplicationController
+
+    def create
+        binding.pry
+    end
+
+    def index
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def recording_params
+        def params.require(:recording).permit(:name, :user_id, :attachment)
+    end
+
 end
