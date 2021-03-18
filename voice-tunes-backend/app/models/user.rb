5ca272b93389249e8356f6876890cf2521cb399c
Alexander M. Clamor <alexander.clamor@gmail.com>
2021-03-17 19:34:33 -0700

    incl commented code to validate username with numbsers

--- voice-tunes-backend/app/models/user.rb
+++ voice-tunes-backend/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
     validates :name,
               presence: { message: "was not entered" },
+            #   format: { with: /\A[a-zA-Z0-9]*\z/, message: "can only contain letters and numbers" },
               format: { with: /\A[a-zA-Z]*\z/, message: "can only contain letters." },
               uniqueness: { case_sensitive: false, message: "is already taken" }
 
